b1087225672ea525041444c049daaf7f01bada72
Mugisha Thierry <thierrymugis@gmail.com>
2020-11-28 10:01:59 +0200

    Added valiables to index route

--- app/main/views.py
+++ app/main/views.py
@@ -11,8 +11,15 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    general = get_news_source('general')
+    entertainment = get_news_source('entertainment')
+    business = get_news_source('business')
+    health = get_news_source('health')
+    sports = get_news_source('sports')
+    science = get_news_source('science')
+    technology = get_news_source('technology')
     title='News source'
-    return render_template('index.html',title=title)
+    return render_template('index.html',title=title,general=general,entertainment=entertainment,business=business,health=health,sports=sports,science=science,technology=technology)
 
 
 
